b3563adbf8a490cbfcb329c46cff867d0a351261
JimizCantCode <97803955+JimizCantCode@users.noreply.github.com>
2022-12-27 15:54:27 -0600

    Added mongoose support

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
-const express = require("./Backend/express").main;
+const express       = require("./Backend/express").main;
+const mongoose      = require('mongoose');
+require('dotenv').config();
+
+
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.MongoURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const connection = mongoose.connection;
+connection.on('error', console.error.bind("Error occurred: "));
+connection.once('open', function() {
+  console.log('Connected to MongoDB');
+});
 
 express();
 
 process.on("SIGINT", () => {
+    connection.close();
     console.log("Shutting off!")
     process.exit(0);
 });
